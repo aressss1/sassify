68520e9b513ad646a927324a890d6094fdcb151d
aressss1 <codewithfaizi@gmail.com>
2023-12-18 11:53:45 +0500

    f-

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,8 @@ import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 const Navbar = () => {
     return (
-        <div className="h-12 mx-20 flex items-center justify-between bg-gradient-to-br from-blue-500 to-purple-700 " >
-            <div className="text-white text-lg font-semibold inter ">
+        <div className="h-16 px-20 flex items-center justify-between bg-gradient-to-r from-blue-500 to-blue-800 " >
+            <div className="text-white text-xl font-semibold inter ">
                 Saasify
             </div>
             <div className="hidden md:flex lg:gap-5 items-center text-white text-base font-normal inter  ">
@@ -15,26 +15,28 @@ const Navbar = () => {
                 <p>Pricing</p>
                 <p>About us</p>
             </div>
-            <Button className='rounded-3xl bg-white text-black font-semibold text-lg' size='default' >
-                Sign up
-            </Button>
-            {/* Responsive Navbar / Hambergur Menu */}
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button variant="outline" size='icon' className='md:hidden text-white' >
-                        <Menu />
-                    </Button>
-                </SheetTrigger>
-                <SheetContent>
-                    <div className="flex flex-col gap-5 items-center text-white text-base font-normal inter  ">
-                        <p>Product</p>
-                        <p>Solution</p>
-                        <p>Customer</p>
-                        <p>Pricing</p>
-                        <p>About us</p>
-                    </div>
-                </SheetContent>
-            </Sheet>
+            <div>
+                <Button className='rounded-3xl bg-white hover: bg-white/80 text-black font-semibold text-lg' size='default' >
+                    Sign up
+                </Button>
+                {/* Responsive Navbar / Hambergur Menu */}
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size='icon' className='md:hidden text-white hover:text-white/75' >
+                            <Menu />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <div className="flex flex-col gap-5 items-center text-white text-base font-normal inter  ">
+                            <p>Product</p>
+                            <p>Solution</p>
+                            <p>Customer</p>
+                            <p>Pricing</p>
+                            <p>About us</p>
+                        </div>
+                    </SheetContent>
+                </Sheet>
+            </div>
         </div>
     );
 }
