6c613566626cf20493d22a14cfca67cf333993fc
aressss1 <codewithfaizi@gmail.com>
2023-12-19 11:35:16 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,35 +3,39 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
-{/* <div className="bg-[url('/bg-image.png')] h-48" /> */}
+{/* <div className="bg-[url('/bg-image.png')] h-[72rem]" /> */ }
 const HeroSection = () => {
     return (
-            <div className="flex flex-col gap-20  lg:px-20 md:px-14 px-8 py-10 relative " >
-                <div className="flex flex-col items-center justify-center pt-20 md:gap-6 inter bg-[url('/bg-image.png')] h-[72rem] " >
-                    <div className=" text-center text-white text-6xl font-extrabold capitalize lg:leading-[4.5rem]">
-                        The fastest way for startups <br /> to do any analysis
-                    </div>
-                    <div className=" lg:px-56 opacity-80 text-center text-white text-xl font-medium leading-normal">
-                        Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration.
-                    </div>
-                    <div className="flex bg-white lg:w-96 p-2 rounded-3xl " >
-                        <div className="flex items-center pl-3" >
-                            <Mail className="text-muted-foreground" />
-                            <Input placeholder="Enter your email " className="focus:ring-0 " />
+        <>
+            <div className="bg-[url('/bg-image.png')] h-[72rem]" >
+                <div className="flex flex-col gap-20  lg:px-20 md:px-14 px-8 py-10 relative " >
+                    <div className="flex flex-col items-center justify-center pt-20 md:gap-6 inter  " >
+                        <div className=" text-center text-white text-6xl font-extrabold capitalize lg:leading-[4.5rem]">
+                            The fastest way for startups <br /> to do any analysis
                         </div>
-                        <Button className='rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold text-base' size='default' >
-                            Start for free
-                        </Button>
+                        <div className=" lg:px-56 opacity-80 text-center text-white text-xl font-medium leading-normal">
+                            Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration.
+                        </div>
+                        <div className="flex bg-white lg:w-96 p-2 rounded-3xl " >
+                            <div className="flex items-center pl-3" >
+                                <Mail className="text-muted-foreground" />
+                                <Input placeholder="Enter your email " className="focus:ring-0 " />
+                            </div>
+                            <Button className='rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold text-base' size='default' >
+                                Start for free
+                            </Button>
+                        </div>
                     </div>
-                </div>
-                <div className="shadow-2xl " >
-                    <Image
-                        src="/hero-image-3.png"
-                        alt="dasboard"
-                        className="rounded-lg w-[75rem] "
-                    />
+                    <div className="shadow-2xl " >
+                        <Image
+                            src="/hero-image-3.png"
+                            alt="dasboard"
+                            className="rounded-lg w-[75rem] "
+                        />
+                    </div>
                 </div>
             </div>
+        </>
     );
 }
 
