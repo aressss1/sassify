8aa6d8a3fd1ed94ae861da58fd2f9e79d95920b9
aressss1 <codewithfaizi@gmail.com>
2023-12-19 12:02:28 +0500

    f-

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar";
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('/bg-image.png')] h-[72rem]" >
+      <div className="bg-[url('/bg-image.png')] lg:h-[72rem] md:[45rem] " >
         <Navbar />
         <HeroSection />
       </div>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -8,10 +8,10 @@ const HeroSection = () => {
     return (
                 <div className="flex flex-col gap-20  lg:px-20 md:px-14 px-8 py-10 relative " >
                     <div className="flex flex-col items-center justify-center pt-20 md:gap-6 inter  " >
-                        <div className=" text-center text-white text-6xl font-extrabold capitalize lg:leading-[4.5rem]">
+                        <div className=" text-center text-white lg:text-6xl md:text-4xl text-lg font-extrabold capitalize lg:leading-[4.5rem]">
                             The fastest way for startups <br /> to do any analysis
                         </div>
-                        <div className=" lg:px-56 opacity-80 text-center text-white text-xl font-medium leading-normal">
+                        <div className="md:px-56 opacity-80 text-center text-white lg:text-xl md:text-base text-xs font-medium leading-normal">
                             Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration.
                         </div>
                         <div className="flex bg-white lg:w-96 p-2 rounded-3xl " >
@@ -19,7 +19,7 @@ const HeroSection = () => {
                                 <Mail className="text-muted-foreground" />
                                 <Input placeholder="Enter your email " className="focus:ring-0 " />
                             </div>
-                            <Button className='rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold text-base' size='default' >
+                            <Button className='rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold md:text-base text-sm' size='default' >
                                 Start for free
                             </Button>
                         </div>
