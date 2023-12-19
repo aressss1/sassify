a698caa5f23418ca51480b9c14d53b5f50b8b101
aressss1 <codewithfaizi@gmail.com>
2023-12-19 10:00:15 +0500

    f-

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,16 +5,16 @@ import { Button } from "./ui/button";
 const HeroSection = () => {
     return (
         <div className="flex flex-col gap-6  lg:px-20 md:px-14 px-8 md:py-10 py-7 bg-[url('/bg-image.png')] " >
-            <div className="flex flex-col mx-auto md:gap-10 gap-16 inter " >
-                <div className=" text-center text-white text-5xl font-extrabold capitalize lg:leading-[4.5rem]">
+            <div className="flex flex-col items-center justify-center mx-auto md:gap-10 gap-16 inter " >
+                <div className=" text-center text-white text-6xl font-extrabold capitalize lg:leading-[4.5rem]">
                     The fastest way for startups to do any analysis
                 </div>
-                <div className="opacity-80 text-center text-white text-xl font-medium leading-loose">\
+                <div className=" lg:px-56 opacity-80 text-center text-white text-xl font-medium leading-loose">\
                     Equals is the only spreadsheet with built-in connections to any database, versioning, and collaboration.
                 </div>
-                <div className="flex bg-white " >
-                    <div className="flex gap-3" >
-                        <Mail />
+                <div className="flex bg-white lg:w-96 p-2 rounded-3xl " >
+                    <div className="flex items-center gap-3" >
+                        <Mail className="text-muted-foreground" />
                         <Input placeholder="Enter your email " />
                     </div>
                     <Button className='rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold text-base' size='default' >
