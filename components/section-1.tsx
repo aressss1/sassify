f796b9c8ebfe3b0d7b0bea3f399c7586f828d65b
aressss1 <codewithfaizi@gmail.com>
2023-12-20 16:44:42 +0500

    f-

--- components/section-1.tsx
+++ components/section-1.tsx
@@ -3,8 +3,8 @@ import { Button } from "./ui/button";
 
 const Section1 = () => {
     return (
-        <div className="bg-violet-50 flex md:flex-row flex-col-reverse items-center justify-around lg:px-20 md:px-14 px-4 lg:py-16 md:py-6" >
-            <div className="flex flex-col md:w-[29rem] gap-7 p-5" >
+        <div className="bg-violet-50 flex md:flex-row flex-col-reverse items-center justify-around lg:px-20 md:px-14 px-4 lg:py-16 py-6" >
+            <div className="flex flex-col md:w-[29rem] md:gap-7 gap-5 p-5" >
                 <div className=" text-black lg:text-5xl md:text-3xl text-xl font-bold inter">
                     Powerful and easy to use SaaS platform
                 </div>
