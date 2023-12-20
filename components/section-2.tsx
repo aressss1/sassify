99dd90f458366d55cd2d4ba422c932619110fe5a
aressss1 <codewithfaizi@gmail.com>
2023-12-20 17:12:58 +0500

    f-

--- components/section-2.tsx
+++ components/section-2.tsx
@@ -4,24 +4,24 @@ import { Button } from "./ui/button";
 const Section2 = () => {
     const boardingPrcoess = [
         {
-            id: 1,
+            stepImageSrc: "/1.png",
             stepName: "Create Account",
             stepDescription: "After learning about your customer service needs, we will get you up"
         },
         {
-            id: 2,
+            stepImageSrc: "/2.png",
             stepName: "Install tracking code",
             stepDescription: "After learning about your customer service needs, we will get you up"
         },
         {
-            id: 3,
+            stepImageSrc: "/3.png",
             stepName: "Track analytics",
             stepDescription: "After learning about your customer service needs, we will get you up "
         },
     ]
 
     return (
-        <div className="bg-[url('/gradient.png')] lg:px-20 md:px-14 px-4 lg:py-16 md:py-10 py-2 flex flex-col items-center " >
+        <div className="bg-[url('/gradient.png')] lg:px-20 md:px-14 px-4 lg:py-16 md:py-10 py-2 flex flex-col " >
             <div className="flex justify-between" >
                 <div className=" text-white text-5xl font-bold inter ">
                     Work smarter<br />with easy access for user..
@@ -30,18 +30,23 @@ const Section2 = () => {
                     Get Started Now
                 </Button>
             </div>
-            <div className="flex flex-row justify-between" >
+            <div className="flex flex-row justify-between pt-24 " >
                 <div className="flex flex-col items-center justify-center gap-5" >
                     {boardingPrcoess.map((step) => (
-                        <div key={step.id} className="flex items-start justify-center " >
-                            <div className="w-24 h-24 bg-white rounded-full text-center " >
-                                {step.id}
+                        <div key={step.stepName} className="flex items-start justify-center " >
+                            <div className="w-20 h-12 bg-white rounded-full text-center " >
+                                <Image 
+                                    src={step.stepImageSrc}
+                                    alt="step"
+                                />
                             </div>
-                            <div className="text-white text-2xl font-bold inter">
-                                {step.stepName}
-                            </div>
-                            <div className=" opacity-80 text-white text-lg font-medium inter ">
-                                {step.stepDescription}
+                            <div className="flex flex-col justify-start" >
+                                <div className="text-white text-2xl font-bold inter">
+                                    {step.stepName}
+                                </div>
+                                <div className=" opacity-80 text-white text-lg font-medium inter ">
+                                    {step.stepDescription}
+                                </div>
                             </div>
                         </div >
                     ))}
