29e5ab2c00916269d47be0544d0e21388f97c6dc
aressss1 <codewithfaizi@gmail.com>
2023-12-21 09:59:17 +0500

    f-

--- components/section-2.tsx
+++ components/section-2.tsx
@@ -23,35 +23,36 @@ const Section2 = () => {
     return (
         <div className="bg-[url('/gradient.png')] lg:px-20 md:px-14 px-4 lg:py-16 md:py-10 py-2 flex flex-col " >
             <div className="flex justify-between" >
-                <div className=" text-white text-5xl font-bold inter ">
+                <div className=" text-white lg:text-5xl md:text-3xl text-2xl font-bold inter ">
                     Work smarter<br />with easy access for user..
                 </div>
-                <Button className='w-40  rounded-3xl bg-white text-blue-800 hover:text-blue-800/75 font-semibold md:text-base text-xs' size='default' >
+                <Button className='md:w-40 w-28  rounded-3xl bg-white hover:bg-white text-blue-800 hover:text-blue-800/75 font-semibold md:text-base text-xs' size='default' >
                     Get Started Now
                 </Button>
             </div>
-            <div className="flex flex-row justify-between pt-24 " >
+            <div className="flex md:flex-row flex-col-reverse justify-between pt-20 " >
                 <div className="flex flex-col items-center justify-center gap-5" >
                     {boardingPrcoess.map((step) => (
                         <div key={step.stepName} className="flex  justify-start  " >
-                            <div className="w-20 h-12 " >
+                            <div >
                                 <Image 
                                     src={step.stepImageSrc}
                                     alt="step"
+                                    className="lg:h-20 md:h-[60px] h-10"
                                 />
                             </div>
-                            <div className="flex flex-col justify-start w-[25rem]" >
-                                <div className="text-white text-2xl font-bold inter">
+                            <div className="flex flex-col justify-start lg:w-[25rem] md:w-72 " >
+                                <div className="text-white lg:text-2xl text-xl font-bold inter">
                                     {step.stepName}
                                 </div>
-                                <div className=" opacity-80 text-white text-lg font-medium inter ">
+                                <div className=" opacity-80 text-white lg:text-lg md:text-base text-sm font-medium inter ">
                                     {step.stepDescription}
                                 </div>
                             </div>
                         </div >
                     ))}
                 </div>
-                <div>
+                <div className="flex items-center" >
                     <Image
                         src="/section-image-2.png"
                         alt="section-image"
