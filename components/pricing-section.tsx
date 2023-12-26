6b13e76f75a1a12cf889863a90a4dc48d63799b2
aressss1 <codewithfaizi@gmail.com>
2023-12-26 09:54:34 +0500

    f-

--- components/pricing-section.tsx
+++ components/pricing-section.tsx
@@ -47,6 +47,7 @@ const PricingSection = () => {
 
     const pricingContentAnnualy = [
         {
+            id: 1,
             name: "Small Business",
             type: "Pro",
             price: "288",
@@ -57,6 +58,7 @@ const PricingSection = () => {
             ]
         },
         {
+            id: 2,
             name: "Medium Business",
             type: "Popular",
             price: "576",
@@ -68,6 +70,7 @@ const PricingSection = () => {
             ]
         },
         {
+            id: 3,
             name: "Enterprise",
             type: "Advance",
             price: "960",
@@ -91,7 +94,7 @@ const PricingSection = () => {
             <Tabs defaultValue="monthly" className="flex flex-col items-center gap-9" >
                 <TabsList>
                     <TabsTrigger value="monthly">Monthly</TabsTrigger>
-                    <TabsTrigger value="Annualy">Annualy</TabsTrigger>
+                    <TabsTrigger value="annualy">Annualy</TabsTrigger>
                 </TabsList>
 
                 <TabsContent value="monthly" >
@@ -100,7 +103,7 @@ const PricingSection = () => {
                             <div key={price.name} className="group lg:w-[328px] md:w-[200px]"  >
                                 <Card className={` 
                                 ${price.id === 2 ? "bg-[url('/gradient.png')]" : "bg-white"} 
-                                flex flex-col gap-4 md:p-2  lg:p-3 p-2  shadow-2xl  rounded-xl h-[275px] md:h-[526px] lg:h-auto ` } >
+                                flex flex-col gap-4 md:p-2  lg:p-3 p-2  shadow-2xl  rounded-xl  md:h-[526px] lg:h-auto ` } >
                                     <CardHeader className="flex flex-col  lg:gap-2 md:gap-0 items-start lg:p-6 p-4" >
                                         <div className={` ${price.id === 2 ? "text-white" : "text-black"}  lg:text-2xl text-xl font-bold inter `}>
                                             {price.type}
@@ -128,7 +131,7 @@ const PricingSection = () => {
                                         {price.content.map((content) => (
                                             <div key={content} className="flex flex-row items-start gap-1 " >
                                                 <CheckCircle2 className={` 
-                                                ${price.id === 2 ? "bg-white" : "bg-blue-800"} 
+                                                ${price.id === 2 ? "bg-transparent" : "bg-blue-800"} 
                                                 rounded-[1.25rem] text-white lg:h-auto lg:w-auto h-5 w-5 `} />
                                                 <p className={`
                                                 ${price.id === 2 ? "text-white" : "text-black"}
@@ -151,38 +154,54 @@ const PricingSection = () => {
                     </div>
                 </TabsContent>
 
-                <TabsContent value="Annualy" >
-                    <div className="flex md:flex-row justify-center lg:gap-12 gap-4 px-5 pb-10 " >
+                <TabsContent value="annually" >
+                    <div className="flex md:flex-row flex-col justify-center lg:gap-12 gap-4 px-5 pb-10" >
                         {pricingContentAnnualy.map((price) => (
-                            <div key={price.name} className="group w-[328px] "  >
-                                <Card className="flex flex-col gap-4 md:p-2  lg:p-3 p-2  shadow-2xl  rounded-xl h-[275px] md:h-[302px] lg:h-auto " >
+                            <div key={price.name} className="group lg:w-[328px] md:w-[200px]"  >
+                                <Card className={` 
+                                ${price.id === 2 ? "bg-[url('/gradient.png')]" : "bg-white"} 
+                                flex flex-col gap-4 md:p-2  lg:p-3 p-2  shadow-2xl  rounded-xl  md:h-[526px] lg:h-auto ` } >
                                     <CardHeader className="flex flex-col  lg:gap-2 md:gap-0 items-start lg:p-6 p-4" >
-                                        <div className=" text-black text-2xl font-bold inter">
+                                        <div className={` ${price.id === 2 ? "text-white" : "text-black"}  lg:text-2xl text-xl font-bold inter `}>
                                             {price.type}
                                         </div>
-                                        <div className="text-blue-800 text-6xl font-bold inter leading-10 flex gap-1 ">
+                                        <div className={`
+                                            ${price.id === 2 ? "text-white" : "text-blue-800"} 
+                                            lg:text-6xl md:text-4xl text-2xl font-bold inter leading-10 flex gap-1 `}>
                                             ${price.price}
-                                            <div className="self-end opacity-60 text-black text-base font-medium inter tracking-tight">
-                                                /annum
+                                            <div className={` 
+                                                ${price.id === 2 ? "text-white" : "text-black"} 
+                                                md:self-end self-center opacity-60  text-base font-medium inter tracking-tight `}>
+                                                /month
                                             </div>
                                         </div>
-                                        <div className="text-black text-base font-normal inter">
+                                        <div className={
+                                            `${price.id === 2 ? "text-white" : "text-black"} 
+                                            lg:text-base text-sm font-normal inter`}>
                                             For {price.name}
                                         </div>
                                     </CardHeader>
                                     <Separator />
-                                    <CardDescription className=" w-[213px] md:w-[185px] lg:w-auto h-[240px] flex flex-col gap-3 text-left text-black lg:text-base md:text-sm font-normal inter lg:p-6 md:p-4 px-2 ">
+                                    <CardDescription className={` 
+                                        ${price.id === 2 ? "text-white" : "text-black"}
+                                        w-[213px] lg:w-auto md:h-[240px] flex flex-col md:gap-3 gap-4 text-left  lg:text-base md:text-sm font-normal inter lg:p-6 md:p-4 px-2 `}>
                                         {price.content.map((content) => (
                                             <div key={content} className="flex flex-row items-start gap-1 " >
-                                                <CheckCircle2 />
-                                                <p className="text-black text-sm font-medium inter tracking-tight" >
+                                                <CheckCircle2 className={` 
+                                                ${price.id === 2 ? "bg-transparent" : "bg-blue-800"} 
+                                                rounded-[1.25rem] text-white lg:h-auto lg:w-auto h-5 w-5 `} />
+                                                <p className={`
+                                                ${price.id === 2 ? "text-white" : "text-black"}
+                                                text-sm font-medium inter tracking-tight `} >
                                                     {content}
                                                 </p>
                                             </div>
                                         ))}
                                     </CardDescription>
                                     <CardFooter className="flex gap-2 justify-center items-center lg:p-6 md:p-4 p-2" >
-                                        <Button className='w-36  rounded-3xl  bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white font-semibold md:text-base text-xs' size='default' >
+                                        <Button className={` 
+                                        ${price.id === 2 ? "bg-white hover:bg-white text-blue-800  hover:text-blue-800/75 " : "bg-gradient-to-r from-blue-500 to-blue-800 hover:text-white/75  text-white"}
+                                        w-36  rounded-3xl   font-semibold md:text-base text-xs `} size='default' >
                                             Get Started
                                         </Button>
                                     </CardFooter>
@@ -191,6 +210,8 @@ const PricingSection = () => {
                         ))}
                     </div>
                 </TabsContent>
+
+
             </Tabs >
         </div>
 
